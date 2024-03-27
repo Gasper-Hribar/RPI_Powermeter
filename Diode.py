fd1a236c4cf0c8372c09a48f2946de08fd65c930
Gasper-Hribar <hribar.gasper@hotmail.com>
2024-03-27 14:28:41 +0100

    Calibration fixed.

--- Diode.py
+++ Diode.py
@@ -350,7 +350,7 @@ class Diode:
                             if self.wavelength in Diode.specific_wavelengths:
                                 self.power_read = self.calibration['diodes'][f'{self.name}']['specific corrections'][f'{self.wavelength}'][f'{int(self.amp_bit_dg408)}'] * self.power_read
 
-                            self.power_read = 2 * (self.multiply_factor * self.power_read * self.calibration['diode ports'][f'{hex(self.adc_add)}'])
+                            self.power_read = 2 * (self.multiply_factor * self.power_read * self.calibration['diode ports'][f'{hex(self.adc_add)}'] * self.calibration['amplificaton calibration'][f'{self.amp_bit_dg408}'])
 
                             if self.multiply_factor > 0:
                                 ratio_pow = 1 / self.power_read
